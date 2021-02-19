e0970e9ec5be454b8de9e2995c3d9325ceaafcb1
Buffaløø <45590407+Bufaloo@users.noreply.github.com>
2021-02-19 17:56:50 +0100

    little big fix

--- WorldVisit_Contest/app/src/main/java/com/example/worldvisit_contest/MainActivity.java
+++ WorldVisit_Contest/app/src/main/java/com/example/worldvisit_contest/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ajouter_pays);
+        setContentView(R.layout.main_activity);
     }
 
     public void Back(View v) {
