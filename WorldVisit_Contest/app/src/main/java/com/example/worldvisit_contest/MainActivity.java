de19c275d82e566e0a31fbe1fc7a5e2661261843
ThibaultLl <45203845+ThibaultLl@users.noreply.github.com>
2021-02-19 18:33:37 +0100

    pushnul

--- WorldVisit_Contest/app/src/main/java/com/example/worldvisit_contest/MainActivity.java
+++ WorldVisit_Contest/app/src/main/java/com/example/worldvisit_contest/MainActivity.java
@@ -3,23 +3,23 @@ package com.example.worldvisit_contest;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
-
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        Button next = (Button) findViewById(R.id.button3);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), AjouterPays.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
     }
-
-    public void Back(View v) {
-        //pour retourner a l’activite principale il suffit seulement d’appler la methode finish() qui vas tuer cette activite
-
-        finish() ;
-
-    }
-
 }
